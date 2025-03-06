d74736685754371981afca61bb7ce7afaab413f0
cnk <cnkgit@qq.com>
2025-03-06 17:52:25 +0800

    建立项目

---
+++ CertificateStatistic/Models/NavBtn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertificateStatistics.Models
+{
+    public class NavBtn
+    {
+        public string Title { get; set; }
+
+        public string Icon { get; set; }
+
+        public string ViewName { get; set; }
+
+    }
+}
